54b8c7bc8b631ea162c731093530d02783f78ff6
Baibhav <baibhav030501@gmail.com>
2021-12-28 11:29:14 +0530

    initial commit

---
+++ src/components/Footer.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+const Footer = () => {
+  return (
+    <div>
+      <p>
+        Made by : <a href="https://github.com/baibhav0305">Baibhav Panda</a>
+      </p>
+    </div>
+  );
+};
+
+export default Footer;
---
+++ src/components/Input.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Input = () => {
+  return (
+    <div>
+      <p>Some random input</p>
+    </div>
+  );
+};
+
+export default Input;
